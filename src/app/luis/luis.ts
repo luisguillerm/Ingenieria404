ace4db6da1776c4820931bc33c964405bef29e35
Freezsoftwaredebugger <23040118@alumno.utc.edu.mx>
2025-11-19 19:34:35 -0600

    a
    a

---
+++ src/app/luis/luis.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-luis',
+  standalone: true,  // Marcamos como standalone
+  templateUrl: './luis.html',
+  styleUrls: ['./luis.css'],
+})
+export class Luis {}
+
